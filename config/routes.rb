ba6675da2e807cc1d9c9949d16c29607b1517639
ju <jluuiss@gmail.com>
2018-01-02 23:14:23 +0800

    Add views for items//new & items/index for testing, set root to items/index

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  get 'items/index'
 
   resources :items
   resources :tags
 
-  root 'welcome#index'
+  root 'items#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
