efa8af4779997b74e9152ab90657298b6f21f64f
ju <jluuiss@gmail.com>
2018-01-03 22:57:39 +0800

    Add & abstracted forms for new & edit, add all basic CRUD functionality

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,18 +7,40 @@ class ItemsController < ApplicationController
 		@item = Item.new
 	end
 
+	def edit
+		@item = Item.find(params[:id])
+	end
+
 	def create
 		@item = Item.new(items_params)
+		@item.status = "Incomplete"
 
 		if @item.save
-			redirect_to :action => 'index'
+			redirect_to items_path
 		else
 			render 'new'
 		end
 	end
 
+	def update
+		@item = Item.find(params[:id])
+
+		if @item.update(items_params)
+      redirect_to items_path
+    else
+      render 'edit'
+    end
+  end
+
+	def destroy
+		@item = Item.find(params[:id])
+		@item.destroy
+
+		redirect_to items_path
+	end
+
 	private
 		def items_params
-			params.require(:item).permit(:task, :due)
+			params.require(:item).permit(:task, :due, :tag_id)
 		end
 end
